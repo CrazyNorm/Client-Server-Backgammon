a6cab5317c497054dda368dfee06ef67dcb88d66
Daniel Gardiner <daniel.gardiner-2@student.manchester.ac.uk>
2023-10-19 12:22:09 +0100

    Add unit tests for Point class

---
+++ DominoBackgammonServer/test/game/board/PointTest.java
@@ -0,0 +1,37 @@
+package game.board;
+
+import game.common.Player;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class PointTest {
+
+    @Test
+    void testClosedConstructor() {
+        Point testPoint = new Point(3, Player.One);
+        assertEquals(testPoint.isClosed(), true);
+    }
+
+    @Test
+    void testBlotConstructor() {
+        Point testPoint = new Point(1, Player.One);
+        assertEquals(testPoint.isBlot(), true);
+    }
+
+    @Test
+    void testOpenConstructor() {
+        Point testPoint = new Point(0, Player.One);
+        assertEquals(testPoint.isOpen(), true);
+    }
+
+    @Test
+    void testDefaultConstructor() {
+        Point testPoint = new Point();
+
+        assertEquals(testPoint.getCount(), 0);
+        assertEquals(testPoint.getPlayer(), Player.None);
+        assertEquals(testPoint.isOpen(), true);
+    }
+}
