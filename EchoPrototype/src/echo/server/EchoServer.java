435c1fd98174018f600204ea70fa9e0035942064
Daniel Gardiner <daniel.gardiner-2@student.manchester.ac.uk>
2023-10-26 10:45:31 +0100

    Update echo server to handle multiple clients

--- EchoPrototype/src/echo/server/EchoServer.java
+++ EchoPrototype/src/echo/server/EchoServer.java
@@ -11,21 +11,44 @@ public class EchoServer {
 
     private final static int PORT = 8081;
 
+    private static boolean listening = true;
+
     public static void main(String[] args) {
         try (
-                ServerSocket serverSocket = new ServerSocket(PORT);
-                Socket clientSocket = serverSocket.accept();
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+                ServerSocket serverSocket = new ServerSocket(PORT)
         ) {
-            String inLine;
-            while ((inLine = in.readLine()) != null) {
-                out.println(inLine);
+            while (listening) {
+                new EchoThread(serverSocket.accept()).start();
             }
-
         } catch (IOException e) {
             System.err.println("Exception caught listening on port " + PORT + " or listening for connection");
             System.err.println(e.getMessage());
+        }
+    }
+
+
+    private static class EchoThread extends Thread {
+
+        private final Socket socket;
+
+        public EchoThread(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try (
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+            ) {
+                String inLine;
+                while ((inLine = in.readLine()) != null) {
+                    out.println(inLine);
+                }
+            } catch (IOException e) {
+                System.err.println("Exception caught listening on port " + PORT + " or listening for connection");
+                System.err.println(e.getMessage());
+            }
         }
     }
 }
