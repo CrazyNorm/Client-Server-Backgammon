bb3f43b8ec81e1626045ec50bff606d35ddffd52
Daniel Gardiner <daniel.gardiner-2@student.manchester.ac.uk>
2024-01-29 13:06:06 +0000

    Implement client connection & simple game matching

--- DominoBackgammonServer/src/server/DBGServer.java
+++ DominoBackgammonServer/src/server/DBGServer.java
@@ -1,32 +1,60 @@
 package server;
 
 import game.common.Game;
+import game.common.Player;
 
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class DBGServer {
 
     private final static int PORT = 8081;
     private static boolean listening = true;
 
-    private static List<ServerThread> threadList = new ArrayList<>();
+    private final static Random RAND = new Random();
 
-    private static List<ServerThread> waiting = new ArrayList<>();
+    private final static List<ServerThread> threadList = new ArrayList<>();
+
+    private final static Queue<ServerThread> queue = new ArrayDeque<>();
 
     public static void main(String[] args) {
         try(ServerSocket serverSocket = new ServerSocket(PORT)) {
             while (listening) {
                 ServerThread tempThread = new ServerThread(serverSocket.accept());
                 threadList.add(tempThread);
-                tempThread.setGame(new Game());
                 tempThread.start();
             }
         } catch (IOException e) {
             System.err.println("Exception listening on port " + PORT);
             System.err.println(e.getMessage());
         }
+    }
+
+    public static void joinQueue(ServerThread thread) {
+        // join queue for game matching
+        // setup ai games
+
+        // if 2 players in queue, start game between them
+        if (!queue.isEmpty()) {
+            ServerThread opponent = queue.poll();
+            thread.addPlayer(opponent.getSocket(), opponent.getPlayerName());
+            opponent.addPlayer(thread.getSocket(), thread.getPlayerName());
+
+            // decide colours at random
+            Player colour = Player.None;
+            while (colour == Player.None)
+                colour = Player.values()[RAND.nextInt(Player.values().length)];
+            Player opColour;
+            if (colour == Player.White) opColour = Player.Black;
+            else opColour = Player.White;
+
+            // initialise game for both players
+            Game game = new Game();
+            thread.startGame(colour, game);
+            opponent.startGame(opColour, game);
+        }
+        else
+            queue.add(thread);
     }
 }
--- DominoBackgammonServer/src/server/ServerThread.java
+++ DominoBackgammonServer/src/server/ServerThread.java
@@ -1,10 +1,8 @@
 package server;
 
 import game.common.Game;
-import server.pojo.Approve;
-import server.pojo.Deny;
-import server.pojo.Message;
-import server.pojo.Response;
+import game.common.Player;
+import server.pojo.*;
 import server.xml.ProtocolMapper;
 
 import java.io.BufferedReader;
@@ -23,6 +21,10 @@ public class ServerThread extends Thread {
 
     private final ProtocolMapper protocolMapper = new ProtocolMapper();
 
+    private String name;
+    private String opponent;
+
+    private Player colour;
     private Game game;
 
     public ServerThread(Socket socket) {
@@ -30,12 +32,23 @@ public class ServerThread extends Thread {
         this.otherPlayers = new ArrayList<>();
     }
 
+    public void addPlayer(Socket socket, String name) {
+        // used to add an opponent
+        otherPlayers.add(socket);
+        opponent = name;
+    }
+
     public void addPlayer(Socket socket) {
+        // used to add spectators
         otherPlayers.add(socket);
     }
 
-    public void setGame(Game game) {
-        this.game = game;
+    public String getPlayerName() {
+        return name;
+    }
+
+    public Socket getSocket() {
+        return socket;
     }
 
 
@@ -45,7 +58,24 @@ public class ServerThread extends Thread {
                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
         ) {
+            // wait to recieve connect message before continuing
+            connect(out, in);
 
+            // wait for server to start the game
+            if (game == null)
+                synchronized (socket) {
+                    socket.wait();
+                }
+            // send start message
+            Message start = new Message();
+            start.setStart(new Start(opponent, colour));
+
+            String start_xml = protocolMapper.serialize(start);
+            out.println(start_xml.split("\n").length + "m");
+            out.println(start_xml);
+            // todo: wait for acknowledge response
+
+            // main server loop
             String lines;
             while((lines = in.readLine()) != null) {
                 if (lines.isEmpty()) continue;
@@ -68,7 +98,9 @@ public class ServerThread extends Thread {
 
                     // find appropriate response
                     Response r = new Response(m.getIdempotencyKey());
-                    if (!m.isTurn()) r.setDeny(new Deny("Wrong message"));
+                    if (game == null) r.setDeny(new Deny("Game hasn't started"));
+                    else if (!m.isTurn()) r.setDeny(new Deny("Wrong message"));
+                    else if (m.getTurn().getPlayer() != colour) r.setDeny(new Deny("Wrong player"));
                     else if (!game.checkTurn(m.getTurn())) r.setDeny(new Deny("Invalid turn"));
                     else r.setApprove(new Approve());
 
@@ -102,6 +134,73 @@ public class ServerThread extends Thread {
         } catch (IOException e) {
             System.err.println("Exception listening for connection");
             System.err.println(e.getMessage());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void connect(PrintWriter out, BufferedReader in) throws IOException {
+        // waits to recieve a 'connect' message from the new client
+        String lines;
+        while((lines = in.readLine()) != null) {
+            if (lines.isEmpty()) continue;
+
+            // get message length & type
+            String type = lines.substring(lines.length() - 1);
+            lines = lines.substring(0, lines.length() - 1);
+
+            // read message
+            StringBuilder doc = new StringBuilder();
+            for (int i = 0; i < Integer.parseInt(lines); i++) {
+                String inLine = in.readLine();
+                inLine = inLine.replaceAll("\n", "").strip();
+                doc.append(inLine);
+            }
+
+            // message type
+            if (type.equals("m")) {
+                Message message = protocolMapper.deserializeMessage(doc.toString());
+
+                // find appropriate response
+                Response r = new Response(message.getIdempotencyKey());
+                if (!message.isConnect()) r.setDeny(new Deny("Connect first"));
+                else r.setApprove(new Approve());
+
+                // send response
+                String xml = protocolMapper.serialize(r);
+                out.println(xml.split("\n").length + "r");
+                out.println(xml);
+
+                // once connect is received, register with server then stop waiting
+                if (r.isApprove()) {
+                    name = message.getConnect().getPlayerName();
+                    DBGServer.joinQueue(this);
+                    break;
+                }
+            }
+
+            // response type
+            else if (type.equals("r")) {
+                Response response = protocolMapper.deserializeResponse(doc.toString());
+
+                // response is reject as only accepting 'connect' messages
+                Response r = new Response(response.getResponseTo());
+                r.setDeny(new Deny("Connect first"));
+
+                // send response
+                String xml = protocolMapper.serialize(r);
+                out.println(xml.split("\n").length + "r");
+                out.println(xml);
+            }
+        }
+    }
+
+
+    public void startGame(Player colour, Game game) {
+        this.colour = colour;
+        this.game = game;
+        synchronized (socket) {
+            socket.notify();
         }
     }
 }
