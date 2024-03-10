188318dd4ab919fcfd07a3f45225406d47b686d8
Daniel Gardiner <daniel.gardiner-2@student.manchester.ac.uk>
2024-03-10 21:01:05 +0000

    Update AI regex

--- DominoBackgammonServer/src/server/ServerThread.java
+++ DominoBackgammonServer/src/server/ServerThread.java
@@ -177,7 +177,7 @@ public class ServerThread extends Thread {
                 else if (!message.isConnect()) r.setDeny(new Deny("Connect first"));
                 else if (message.getConnect().getOpponentType().startsWith("ai:")){
                     String opponentType = message.getConnect().getOpponentType();
-                    if (!opponentType.matches("^ai:(random|(minimax|mcts)-[1-5])$"))
+                    if (!opponentType.matches("^ai:(random|(minimax-(aggressive|defensive))|(mcts-[1-5]))$"))
                         r.setDeny(new Deny("Invalid AI config"));
                     else if (!DBGServer.isAIAvailable()) r.setDeny(new Deny("AI unavailable"));
                     else r.setApprove(new Approve());
