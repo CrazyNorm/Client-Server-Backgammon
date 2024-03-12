baf56e453a506a18e8613ee2c1ef3d256b331783
Daniel Gardiner <daniel.gardiner-2@student.manchester.ac.uk>
2024-03-12 11:53:25 +0000

    Incorporate opponent hit count into aggressive heuristic

--- DominoBackgammonAI/src/main/java/ai/heursitics/AggressiveHeuristic.java
+++ DominoBackgammonAI/src/main/java/ai/heursitics/AggressiveHeuristic.java
@@ -4,20 +4,26 @@ import game.Game;
 
 public class AggressiveHeuristic extends Heuristic {
 
-    static double hitWeight = 3;
-    static double blockedEntryWeight = 2;
-    static double blockedPointWeight = 1;
+    private static final double hitWeight = 3;
+    private static final double blockedEntryWeight = 2;
+    private static final double blockedPointWeight = 1;
+    private static final double pipCountWeight = 1;
 
     @Override
     public double evaluate(Game game, byte player) {
-        // aggressive heuristic - favours hitting ... todo
+        // aggressive heuristic - favours hitting, blocking points & blocking entry
+        // also favours higher opponent pip count
+
         byte opponent = (byte) -player;
 
         double hitCount = game.checkBar(opponent);
         double blockedEntry = getBlockedEntry(game, player);
         double blockedPoints = getBlockedPoints(game, player) - blockedEntry;
 
+        // represents opponent pip count as a ratio against the starting pip count
+        double pipRatio = (double) game.getPipCount(opponent) / 167;
+
         return player * (hitWeight * hitCount + blockedEntryWeight * blockedEntry
-                + blockedPointWeight * blockedPoints);
+                + blockedPointWeight * blockedPoints + pipCountWeight * pipRatio);
     }
 }
