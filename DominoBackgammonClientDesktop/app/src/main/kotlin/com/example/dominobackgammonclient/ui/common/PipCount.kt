097348fc7c697b5eadc4573ef54ad3ab37fb217a
Daniel Gardiner <daniel.gardiner-2@student.manchester.ac.uk>
2024-03-15 10:51:28 +0000

    Copy common composables

---
+++ DominoBackgammonClientDesktop/app/src/main/kotlin/com/example/dominobackgammonclient/ui/common/PipCount.kt
@@ -0,0 +1,65 @@
+package com.example.dominobackgammonclient.ui.common
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import com.example.dominobackgammonclient.ui.board.Piece
+import com.example.dominobackgammonclient.ui.theme.DominoBackgammonClientTheme
+import ui.BGColour
+
+@Composable
+fun PipCount(
+    colour: BGColour,
+    count: Int,
+    modifier: Modifier = Modifier,
+    simple: Boolean = false
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+    ) {
+        Piece(colour)
+        Text(
+            if (simple) " " else "Moves left:",
+            color = MaterialTheme.colorScheme.onSecondary)
+        Text(
+            count.toString(),
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.onSecondary
+        )
+    }
+}
+
+
+@Preview
+@Composable
+fun PreviewWhitePipCount() {
+    DominoBackgammonClientTheme {
+        PipCount(
+            BGColour.WHITE,
+            count = 167,
+            Modifier.background(Color(0xff118811))
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewBlackPipCount() {
+    DominoBackgammonClientTheme {
+        PipCount(
+            BGColour.BLACK,
+            count = 156,
+            Modifier.background(Color(0xff118811))
+        )
+    }
+}
